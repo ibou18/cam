fa2f86b2103c3dabfe07176b3cd9bb053a603bf3
sia <my@pers.on.al>
2022-12-02 17:48:50 -0500

    add new reader

--- src/pages/admin/QrCode.jsx
+++ src/pages/admin/QrCode.jsx
@@ -1,104 +1,60 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { QrReader } from "react-qr-reader";
-import Webcam from "react-webcam";
-
-const videoConstraints = {
-  width: 1280,
-  height: 720,
-  facingMode: "user",
-};
-
-const WebcamCapture = () => (
-  <Webcam
-    audio={false}
-    height={720}
-    screenshotFormat="image/jpeg"
-    width={1280}
-    videoConstraints={videoConstraints}
-  >
-    {({ getScreenshot }) => (
-      <button
-        onClick={() => {
-          const imageSrc = getScreenshot();
-        }}
-      >
-        Capture photo
-      </button>
-    )}
-  </Webcam>
-);
 
 const QrCode = () => {
-  const [code, setCode] = useState(null);
-  const [showDialog, setDiaglog] = useState(false);
-  const [processing, setProcessing] = useState(false);
-  const [precScan, setPrecScan] = useState("");
   const [selected, setSelected] = useState("environment");
-  const [activeScan, setActiveScan] = useState(false);
+  const [startScan, setStartScan] = useState(false);
+  const [loadingScan, setLoadingScan] = useState(false);
+  const [data, setData] = useState("");
 
-  let datas = ["https://labo.fnac.com/", "www.laguiplus.com"];
-
-  const handleScan = (scanData) => {
-    if (scanData && scanData) {
-      // window.location.href = scanData;
-      console.log(scanData);
+  const handleScan = async (scanData) => {
+    setLoadingScan(true);
+    console.log(`loaded data data`, scanData);
+    if (scanData && scanData !== "") {
+      console.log(`loaded >>>`, scanData);
+      setData(scanData);
+      setStartScan(false);
+      setLoadingScan(false);
+      // setPrecScan(scanData);
     }
   };
   const handleError = (err) => {
     console.error(err);
   };
-
-  const videoRef = useRef(null);
-  const getVideo = () => {
-    navigator.mediaDevices
-      .getUserMedia({ video: { width: 700 } })
-      .then((stream) => {
-        let video = videoRef.current;
-        console.log("stream", stream);
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch((err) => {
-        console.error("error:", err);
-      });
-  };
-
   return (
-    <div className="px-5">
-      <WebcamCapture />
-      {/* <video ref={videoRef} className="mx-auto mt-10" />
-      <canvas /> */}
+    <div className="mx-auto mt-10 p-5 text-center grid gap-y-2">
+      <h1 className="text-center">Scan de billet</h1>
+      <h2>
+        Last Scan:
+        {selected}
+      </h2>
 
-      <p>
-        {" "}
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quasi
-        consectetur recusandae officiis illo officia praesentium ducimus sunt
-        tempora eligendi? Dolore ipsum assumenda possimus deleniti soluta nisi
-        rerum repudiandae quaerat.
-      </p>
-
-      <select onChange={(e) => setSelected(e.target.value)}>
-        <option value={"environment"}>Cámara trasera</option>
-        <option value={"user"}>Cámara delantera</option>
-      </select>
       <button
-        className="bg-teal-500 rounded-lg px-5 py-2 mt-5 ml-10"
+        className="bg-blue-500 text-white px-2 w-36 mx-auto rounded-xl py-1"
         onClick={() => {
-          getVideo();
-          setActiveScan(!activeScan);
+          setStartScan(!startScan);
         }}
       >
-        {" "}
-        Activer le Scan
+        {startScan ? "Stop Scan" : "Start Scan"}
       </button>
-      {!showDialog && !processing && activeScan && (
-        <QrReader
-          facingMode={selected}
-          delay={500}
-          onScan={handleScan}
-          className="qr-reader"
-        />
+      {startScan && (
+        <>
+          <select onChange={(e) => setSelected(e.target.value)}>
+            <option value={"environment"}>Caméra Arrière</option>
+            <option value={"user"}>Caméra Avant</option>
+          </select>
+          <QrReader
+            facingMode={selected}
+            delay={1000}
+            onError={handleError}
+            onScan={handleScan}
+            // chooseDeviceId={()=>selected}
+            style={{ width: "300px" }}
+          />
+        </>
       )}
+      {loadingScan && <p>Loading</p>}
+      {data !== "" && <p>{data}</p>}
     </div>
   );
 };
